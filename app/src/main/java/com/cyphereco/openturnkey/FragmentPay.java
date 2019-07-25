7f7126feda32f5a14b852cda7a455f647efac807
jengjy <jengjy@gmail.com>
2019-07-25 22:05:28 +0800

    Set input recipient address unfocusable so that the hint can be shown correctly.

--- app/src/main/java/com/cyphereco/openturnkey/FragmentPay.java
+++ app/src/main/java/com/cyphereco/openturnkey/FragmentPay.java
@@ -3,6 +3,7 @@ package com.cyphereco.openturnkey;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +16,10 @@ public class FragmentPay extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_pay, container, false);
+        View view = inflater.inflate(R.layout.fragment_pay, container, false);
+        // Set not focusable for recipient address so that the hint is shown correctly
+        TextInputEditText inputReceipientAddress = view.findViewById(R.id.input_recipient_address);
+        inputReceipientAddress.setFocusable(false);
+        return view;
     }
-
 }
