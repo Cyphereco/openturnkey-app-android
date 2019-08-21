d96c0994f8df4f2843e75ad52b565afd9ddb4080
yorotsuya <yorotsuya@protonmail.com>
2019-08-21 15:13:55 +0900

    Update UI modifications

---
+++ app/src/main/java/AddressesItemAdapter.java
@@ -0,0 +1,11 @@
+import android.support.v7.widget.RecyclerView;
+
+public class AddressesItemAdapter {
+    private static final String TAG = "AddressesItemAdapter";
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        
+
+    }
+}
